45938da132ad55ac845c6b31f6b8a8cf9b035053
Michiel Dubbelman <m.p.dubbelman@tudelft.nl>
2025-10-26 17:31:59 +0100

    first commit

---
+++ src/drippy/__init__.py
@@ -0,0 +1,9 @@
+"""Documentation about drippy."""
+
+import logging
+
+logging.getLogger(__name__).addHandler(logging.NullHandler())
+
+__author__ = "Michiel Dubbelman"
+__email__ = "m.p.dubbelman@tudelft.nl"
+__version__ = "0.0.1"
